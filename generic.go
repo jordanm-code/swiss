2bf36d70a778026b1239bda228f184bf483168c5
joeshin <ping.jshin@gmail.com>
2024-05-24 09:12:17 -0400

    implement PtrTo and Deref (#8)

--- generic.go
+++ generic.go
@@ -1,5 +1,19 @@
 package swiss
 
+// PtrTo returns a pointer to the value passed in.
+func PtrTo[T any](v T) *T {
+	return &v
+}
+
+// Deref returns the value that the pointer points to.
+func Deref[T any](v *T) (r T) {
+	if v == nil {
+		return
+	}
+	r = *v
+	return
+}
+
 // Map creates a map from a slice of keys.  The value of each key
 // is a boolean indicating whether the key is present in the slice.
 func Map[S ~[]E, E comparable](s S) map[E]bool {
